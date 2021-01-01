b7f0429d1c7e5e7e1dca674ea3505ca9e322e6cb
Eli Jagnow <eli.jagnow@gmail.com>
2021-01-01 17:19:10 -0600

    Day-3 Part-2 Reformat
    Part-1 code works with the test and advent inputs. It has been reformatted as a function for Part-2.

--- Advent3/src/Advent3.java
+++ Advent3/src/Advent3.java
@@ -7,26 +7,44 @@ public class Advent3
     {
         int treeCount = 0;
         
+        //Call a function to count the number of trees
+        treeCount = calcSlope(3, 1);
+
+        //Print the result
+        System.out.println(treeCount);
+    }
+
+    public static int calcSlope(int rightX, int downY)
+    {
+        int treeCalc = 0;
+
         //Read the file and calcuate the number of trees crashed into
         try(BufferedReader br = new BufferedReader(new FileReader("C:\\dev\\advent-code-2020\\Advent3\\src\\input.txt")))
         {
             String line;
-            int modDist = 0;
+            int modDistX = 0;
+            int modDistY = 0;
 
             while((line = br.readLine()) != null)
             {
-                //Find if a tree appears in the next line location
-                char pos = line.charAt(modDist);
-                if(pos == '#'){ treeCount++; }
+                //Check if the next line is a valid y location
+                if(modDistY == 0)
+                {
+                    //Find if a tree appears in the line x location
+                    char pos = line.charAt(modDistX);
+                    if(pos == '#'){ treeCalc++; }
 
-                //Go to the next location
-                modDist = (modDist + 3) % line.length();
+                    //Go to the next location
+                    modDistX = (modDistX + rightX) % line.length();
+                }
+
+                //Increment to the next line modulus
+                modDistY = (modDistY + 1) % downY;
             }
         }
         catch(Exception e)
         { e.printStackTrace(); }
 
-        //Print the result
-        System.out.println(treeCount);
+        return treeCalc;
     }
 }

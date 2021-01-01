3d8a417271dd83022696a1859c31b6bdb547a53f
Eli Jagnow <eli.jagnow@gmail.com>
2021-01-01 17:04:41 -0600

    Day-3 Part-1 Test Input
    Part-1 code works with test input.

--- Advent3/src/Advent3.java
+++ Advent3/src/Advent3.java
@@ -5,19 +5,28 @@ public class Advent3
 {
     public static void main(String[] args) throws Exception
     {
-        int validPass = 0;
+        int treeCount = 0;
         
-        //Read the file and calcuate the number of valid passwords
-        try(BufferedReader br = new BufferedReader(new FileReader("C:\\dev\\advent-code-2020\\Advent2\\src\\input.txt")))
+        //Read the file and calcuate the number of trees crashed into
+        try(BufferedReader br = new BufferedReader(new FileReader("C:\\dev\\advent-code-2020\\Advent3\\src\\input.txt")))
         {
             String line;
+            int modDist = 0;
 
             while((line = br.readLine()) != null)
             {
+                //Find if a tree appears in the next line location
+                char pos = line.charAt(modDist);
+                if(pos == '#'){ treeCount++; }
+
+                //Go to the next location
+                modDist = (modDist + 3) % line.length();
             }
         }
         catch(Exception e)
         { e.printStackTrace(); }
 
+        //Print the result
+        System.out.println(treeCount);
     }
 }

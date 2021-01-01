ac1076e0e91565ee2f0a629c291c7a52982b37b8
Eli Jagnow <eli.jagnow@gmail.com>
2021-01-01 16:42:11 -0600

    Day-3 Setup
    Laying the foundations for the new code.

---
+++ Advent3/src/Advent3.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Advent3
+{
+    public static void main(String[] args) throws Exception
+    {
+        int validPass = 0;
+        
+        //Read the file and calcuate the number of valid passwords
+        try(BufferedReader br = new BufferedReader(new FileReader("C:\\dev\\advent-code-2020\\Advent2\\src\\input.txt")))
+        {
+            String line;
+
+            while((line = br.readLine()) != null)
+            {
+            }
+        }
+        catch(Exception e)
+        { e.printStackTrace(); }
+
+    }
+}
